7b7be971c34cdcb899052dd692a7eb92f4db61e2
Yash Raj <kumaryashraj21@gmail.com>
2026-06-25 20:36:41 +0530

    fixed db issue

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/auth-db')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected successfully!!")
     } catch (error) {
         console.error("Connection to DB failed");
